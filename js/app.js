1513671614073660638f8de081ae521525f06b61
mardiniii <sezama03@hotmail.com>
2016-11-11 15:39:36 -0500

    Add binding for .option on click event

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 // Define correct option
 var correct = Math.floor(Math.random() * 2);
 
+// Binding a click color
+$('.option').on('click', function() {
+  var index = $('.option').index(this);
+
+  // Verifies answer
+  if ( index == correct ) {
+    alert("Muy bien!!!!!!!");
+  } else {
+    alert("Fallaste!!!!!");
+  }
+});
+
 // Generates Random Colors
 $('.option').each(function(index) {
   var color = generateColor();
